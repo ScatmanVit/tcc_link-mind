7c4ff2e1a3688a540a847b59ef954a37dd589982
Vitor Ribeiro Baradel <victorribeirobaradel@gmail.com>
2025-08-11 17:55:27 -0300

    configurando redis

--- server/config/redis.js
+++ server/config/redis.js
@@ -2,7 +2,11 @@ import Redis from 'ioredis'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const redis = new Redis(process.env.REDIS)
+const redis = new Redis(process.env.REDIS, {
+  tls: undefined, 
+  connectTimeout: 5000,
+  enableOfflineQueue: false,
+})
 
 redis.on('connect', () => {
   console.log('Redis conectado com sucesso!')
