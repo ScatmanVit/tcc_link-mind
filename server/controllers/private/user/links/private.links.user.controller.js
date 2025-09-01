cd1247613e3fbadac0947640086ff2738d764d08
Vitor <victorribeirobaradel@gmail.com>
2025-08-31 21:55:40 -0300

    rota de update de links adcionada

--- server/controllers/private/user/links/private.links.user.controller.js
+++ server/controllers/private/user/links/private.links.user.controller.js
@@ -5,136 +5,190 @@ dotenv.config()
 
 
 async function links_create_Controller_POST(req, res) {
-    try {
-        const userId = req.user?.id
-        if (!userId) {
-            return res.status(401).json({
-                error: "Usuário não autenticado. Token inválido ou ausente."
-            })
-        }
+	const userId = req.user?.id
 
-        const userExist = await findOneUser("", userId)
-        if (!userExist) {
-            return res.status(404).json({
-                error: "Usuário não encontrado. Não é possível criar link."
-            })
-        }
+	try {
+		if (!userId) {
+			return res.status(401).json({
+				error: "Usuário não autenticado. Token inválido ou ausente."
+			})
+		}
 
-        const { title, link, description, categoriaId, estadoId, notification, tagsRelacionadas } = req.body
+		const userExist = await findOneUser("", userId)
+		if (!userExist) {
+			return res.status(404).json({
+				error: "Usuário não encontrado. Não é possível criar link."
+			})
+		}
+		const { 
+			title, 
+			link, 
+			description, 
+			categoriaId,
+			estadoId, 
+			notification, 
+			tagsRelacionadas 
+		} = req.body
+		if (!title || !link) {
+			return res.status(400).json({
+				error: "Campos obrigatórios não fornecidos."
+			})
+		}
+		const newLink = await PrivateUserService.links_CREATE({
+			title,
+			link,
+			description,
+			categoriaId,
+			estadoId,
+			idUser: userId,
+			notification,
+			tagsRelacionadas,
+		})
 
-        if (!title || !link) {
-            return res.status(400).json({
-                error: "Campos obrigatórios não fornecidos."
-            })
-        }
+		if (newLink?.error) {
+			return res.status(400).json({ error: newLink.error })
+		}
 
-
-        const newLink = await PrivateUserService.links_CREATE({
-            title,
-            link,
-            description,
-            categoriaId,
-            estadoId,
-            idUser: userId,
-            notification,
-            tagsRelacionadas,
-        })
-
-        if (newLink?.error) {
-            return res.status(400).json({ error: newLink.error })
-        }
-
-        return res.status(201).json({
-            success: true,
-            message: "Link criado com sucesso!",
-            link: newLink
-        })
-    } catch (err) {
-        console.error("Erro no servidor, [ CRIAÇÃO LINK ]", err)
-        return res.status(500).json({
-            error: "Ocorreu um erro no servidor ao criar o link."
-        })
-    }
+		return res.status(201).json({
+			success: true,
+			message: "Link criado com sucesso!",
+			link: newLink
+		})
+	} catch (err) {
+		console.error("Erro no servidor, [ CRIAÇÃO LINK ]", err)
+		return res.status(500).json({
+			error: "Ocorreu um erro no servidor ao criar o link."
+		})
+	}
 }
 
 
-async function links_delete_Controller_DELETE(req, res){
-    const linkId = req.params.id
-    const idUser = req.user?.id
+async function links_delete_Controller_DELETE(req, res) {
+	const linkId = req.params.id
+	const idUser = req.user?.id
 
-    try {
-        if (!linkId || !idUser) {
-            return res.status(400).json({
-                message: "Id do link e do usuário não foram fornecidos"
-            })
-        }
-        const userExist = await findOneUser("", idUser)
-        if (!userExist) {
-            return res.status(404).json({
-                error: "Não existe usuário com esse ID para o link."
-            })
-        }
+	try {
+		if (!linkId || !idUser) {
+			return res.status(400).json({
+				message: "Id do link e do usuário não foram fornecidos"
+			})
+		}
+		const userExist = await findOneUser("", idUser)
+		if (!userExist) {
+			return res.status(404).json({
+				error: "Não existe usuário com esse ID para o link."
+			})
+		}
 
-        const linkDeleted = await PrivateUserService.link_DELETE(linkId, idUser)
+		const linkDeleted = await PrivateUserService.link_DELETE(linkId, idUser)
 
-        if (linkDeleted.error) {
-            return res.status(400).json({
-                message: linkDeleted.error
-            })
-        }
-        if (linkDeleted) {
-            return res.status(200).json({
-                success: true,
-                message: "Link deletado com sucesso!"
-            })
-        } else {
-            return res.status(404).json({
-                success: false,
-                message: "Link não encontrado"
-            })
-        }
-    } catch(err) {
-        console.error("Ocorreu um erro no servidor [ DELETE LINK ]", err)
-            return res.status(500).json({
-                error: "Ocorreu um erro no servidor"
-        })
-    }
-
+		if (linkDeleted.error) {
+			return res.status(400).json({
+				message: linkDeleted.error
+			})
+		}
+		if (linkDeleted) {
+			return res.status(200).json({
+				success: true,
+				message: "Link deletado com sucesso!"
+			})
+		} else {
+			return res.status(404).json({
+				success: false,
+				message: "Link não encontrado"
+			})
+		}
+	} catch (err) {
+		console.error("Ocorreu um erro no servidor [ DELETE LINK ]", err)
+		return res.status(500).json({
+			error: "Ocorreu um erro no servidor"
+		})
+	}
 }
 
-async function links_list_Controller_GET(req, res){
-    const userId = req.user?.id
-    try {
-        if(!userId) return res.status(401).json({
-            error: "ID Token não fornecido no token."
-        })
-        const userExist = await findOneUser("", userId)
-        if(!userExist) {
-            return res.status(401).json({
-                error: "Não existe usuário com esse ID para listar os Links."
-            })
-        }
-        const linksUser = await PrivateUserService.links_LIST(userId)
-        if(linksUser?.error) {
-            return res.status(400).json({
-                 error: linksUser.error 
-            })
-        } 
-        
-        return res.status(200).json({ 
-            success: true,
-            message: "Links do usuário obtidos com sucesso!", linksUser
-        })
-    } catch(err) {
-        console.error("Erro no servidor, [ LISTAGEM LINKS ]", err)
-        return res.status(500).json({
-            error: "Ocorreu um erro no servidor."
-        })
-    }
+
+async function links_update_Controller_UPDATE(req, res) {
+	const userId = req.user?.id
+	const idLink = req.params?.id
+	const {
+		newLink,
+		newName,
+		newDescription,
+		newCategory,
+		newEstadoId,
+		newTagsRelacionadas,
+		newNotification
+	} = req.body
+	if (!userId || !idLink) {
+		return res.status(400).json({
+			error: "Id no usuário ou do link não fornecidos"
+		})
+	}
+
+	try {
+		const linkUpdated = await PrivateUserService.links_UPDATE({
+			idUser: userId,
+			idLink: idLink,
+			newLink,
+			newName,
+			newDescription,
+			newCategory,
+			newEstadoId,
+			newTagsRelacionadas,
+			newNotification
+		})
+		if (linkUpdated.error) {
+			return res.status(400).json({
+				message: linkUpdated.error
+			})
+		}
+		return res.status(200).json({
+			success: true, 
+			message: "Seu link foi alterado com sucesso!"
+		})
+	} catch (err) {
+		console.error("Ocorreu um erro no servidor, [ UPDATE LINK  ]", err)
+		return res.status(500).json({
+			error: "Ocorreu um erro no servidor."
+		})
+	}
+}
+
+async function links_list_Controller_GET(req, res) {
+	const userId = req.user?.id
+
+	try {
+		if (!userId) return res.status(401).json({
+			error: "ID Token não fornecido no token."
+		})
+		const userExist = await findOneUser("", userId)
+		if (!userExist) {
+			return res.status(401).json({
+				error: "Não existe usuário com esse ID para listar os Links."
+			})
+		}
+		const linksUser = await PrivateUserService.links_LIST(userId)
+		if (linksUser?.error) {
+			return res.status(400).json({
+				error: linksUser.error
+			})
+		}
+
+		return res.status(200).json({
+			success: true,
+			message: "Links do usuário obtidos com sucesso!", linksUser
+		})
+	} catch (err) {
+		console.error("Erro no servidor, [ LISTAGEM LINKS ]", err)
+		return res.status(500).json({
+			error: "Ocorreu um erro no servidor."
+		})
+	}
 }
 
 export default {
-    links_create_Controller_POST,
-    links_list_Controller_GET,
-    links_delete_Controller_DELETE
+	links_create_Controller_POST,
+	links_list_Controller_GET,
+	links_delete_Controller_DELETE,
+	links_update_Controller_UPDATE
 }
--- server/services/private/user/links/private.links.user.service.js
+++ server/services/private/user/links/private.links.user.service.js
@@ -3,98 +3,152 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
 
 async function links_CREATE(dataNewLink) {
-  const {     
-    title, 
-    link, 
-    description, 
-    categoriaId, 
-    estadoId,
-    idUser,
-    notification,
-    tagsRelacionadas
-  } = dataNewLink
+	const {
+		title,
+		link,
+		description,
+		categoriaId,
+		estadoId,
+		idUser,
+		notification,
+		tagsRelacionadas
+	} = dataNewLink
 
-  if (!idUser) {
-    return { error: "Id do usuário não fornecido para a criação do novo link" }
-  }
-  try {
-    const data = {
-      title,
-      link,
-      userId: idUser,
-      description: description || null,
-      notification: notification || null,
-    }
-    if (categoriaId) data.categoriaId = categoriaId
-    if (estadoId) data.estadoId = estadoId
-    if (tagsRelacionadas && tagsRelacionadas.length) {
-      data.tagRelacionadas = {
-        connect: tagsRelacionadas.map(id => ({ id }))
-      }
-    }
-    const newLink = await prisma.link.create({ data })
-    return newLink
-  } catch (err) {
-    console.error(err)
-    return {
-        error: "Não foi possível criar o novo link" 
-    }
-  }
+	if (!idUser) {
+		return { error: "Id do usuário não fornecido para a criação do novo link" }
+	}
+	try {
+		const data = {
+			title,
+			link,
+			userId: idUser,
+			description: description || null,
+			notification: notification || null,
+		}
+		if (categoriaId) data.categoriaId = categoriaId
+		if (estadoId) data.estadoId = estadoId
+		if (tagsRelacionadas && tagsRelacionadas.length) {
+			data.tagRelacionadas = {
+				connect: tagsRelacionadas.map(id => ({ id }))
+			}
+		}
+		const newLink = await prisma.link.create({ data })
+		return newLink
+	} catch (err) {
+		console.error(err)
+		return {
+			error: "Não foi possível criar o novo link"
+		}
+	}
 }
 
-async function link_DELETE(idLink, idUser){
-    if(!idLink || !idUser) {
-        return { 
-            error: "Id do link ou do usuário não fornecidos para deletar o link." 
-        }
-    }
-    try {
-      const linkDeleted = await prisma.link.deleteMany({
-        where: {
-          userId: idUser,
-          id: idLink
-        }
-      })
-      if (linkDeleted.count === 0) {
-        return { 
-          error: "O link não foi deletado" 
-        }
-      }
-      return linkDeleted.count > 0
-    } catch(err) {
-      console.error(err)
-      return { error: "Não foi possível deletar o link" }
-    }
+async function link_DELETE(idLink, idUser) {
+	if (!idLink || !idUser) {
+		return {
+			error: "Id do link ou do usuário não fornecidos para deletar o link."
+		}
+	}
+	try {
+		const linkDeleted = await prisma.link.deleteMany({
+			where: {
+				userId: idUser,
+				id: idLink
+			}
+		})
+		if (linkDeleted.count === 0) {
+			return {
+				error: "O link não foi deletado"
+			}
+		}
+		return linkDeleted.count > 0
+	} catch (err) {
+		console.error(err)
+		return {
+			error: "Não foi possível deletar o link"
+		}
+	}
 }
 
-async function links_LIST(idUser){
-    if(!idUser) {
-        return { 
-            error: "Id do usuário não fornecido para busca dos links do usuário" 
-        }
-    }
-    try {
-        const links = await prisma.link.findMany({
-            where: {
-                userId: idUser
-            }
-        })
-        if(links.length === 0) {
-            return { 
-                error: "Sem links disponíveis para esse id de usuário" 
-            }
-        } 
-        return links
-    } catch(err) {
-        console.error(err) 
-        return { 
-            error: "Não foi possível buscar links do usuário" 
-        }
-    }
+
+async function links_UPDATE(dataUptdatedink) {
+	const {
+		idUser,
+		idLink,
+		newLink,
+		newName,
+		newDescription,
+		newCategory,
+		newEstadoId,
+		newTagsRelacionadas,
+		newNotification
+	} = dataUptdatedink
+
+	if (!idUser || !idLink) {
+		return {
+			error: "Id do usuário ou link não fornecido para alterar o link do usuário"
+		}
+	}
+	try {
+		const newLinkData = Object.fromEntries(
+			Object.entries({
+				title: newName,
+				link: newLink,
+				description: newDescription,
+				categoriaId: newCategory,
+				estadoId: newEstadoId,
+				tagsRelacionadas: newTagsRelacionadas,
+				notification: newNotification,
+			}).filter(([_, value]) => value != null && value !== "")
+		);
+
+		const linkUpdated = await prisma.link.update({
+			where: {
+				userId: idUser,
+				id: idLink
+			},
+			data: newLinkData
+		})
+		return linkUpdated
+	} catch (err) {
+		if (err.code === 'P2025') {
+			return { error: "O link que você tentou alterar não existe"}
+		}
+		console.error(err)
+		return {
+			error: "Não possível alterar o link"
+		}
+	}
+}
+
+async function links_LIST(idUser) {
+	if (!idUser) {
+		return {
+			error: "Id do usuário não fornecido para busca dos links do usuário"
+		}
+	}
+	try {
+		const links = await prisma.link.findMany({
+			where: {
+				userId: idUser
+			}
+		})
+		if (links.length === 0) {
+			return {
+				message: "Sem links disponíveis para esse id de usuário"
+			}
+		}
+		return links
+	} catch (err) {
+		console.error(err)
+		return {
+			error: "Não foi possível buscar links do usuário"
+		}
+	}
 }
 
 export default {
-    links_LIST,
-    links_CREATE,
-    link_DELETE
+	links_LIST,
+	links_CREATE,
+	link_DELETE,
+	links_UPDATE
 }
