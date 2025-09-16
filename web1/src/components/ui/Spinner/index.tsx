8b9bcf186ce2d15bdb19b71b055953faf4821f55
Vitor <victorribeirobaradel@gmail.com>
2025-09-15 23:13:17 -0300

    corrigindo nome da pasta

--- web1/src/components/ui/Spinner/index.tsx
+++
@@ -1,5 +0,0 @@
-import "./style.css";
-export default function Spinner({ size = 18 }: { size?: number }) {
-  const s = `${size}px`;
-  return <span className="spinner" style={{ width: s, height: s }} />;
-}
