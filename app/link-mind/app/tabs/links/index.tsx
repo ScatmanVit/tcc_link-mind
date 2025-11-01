f6b1452ed14b0d9b77e433aa6b67c8c241f58407
Vitor <victorribeirobaradel@gmail.com>
2025-11-01 19:02:16 -0300

    refactor(links): integração com novo modal e ajustes no fluxo de ações

--- app/link-mind/app/tabs/links/index.tsx
+++ app/link-mind/app/tabs/links/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useContext } from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Text } from 'react-native'
 import NetInfo from '@react-native-community/netinfo'
-
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { colors } from '@/styles/colors'
 import { useCategory } from '@/src/components/categories/useCategory'
@@ -14,25 +14,28 @@ import {
 } from '@/src/async-storage/categories'
 
 import LinkSkeleton from '@/src/components/links/linksSkeleton';
-import Links from '@/src/components/links/links'
+import Links, { LinkWithId } from '@/src/components/links/links'
 
 import list_Links from '@/src/services/links/listLinks'
 import delete_Link from '@/src/services/links/deleteLink'
 import category_Create from '@/src/services/categories/createCategories'
 import categories_List from '@/src/services/categories/listCategories'
-import ChooseOptionModal from '@/src/components/modals/chooseOptionModal'
+import ChooseOptionModal from '@/src/components/modals/modalBottomSheet'
+import ActionSelector from '@/src/components/actionSelector';
 
 
 export default function LinksIndex() {
     const { user } = useContext(AuthContext) // provisório pegar do estado o token, não da para usar o secure store no web
     const { selectedCategory, setSelectCategory } = useCategory()
-
+ 
+    const [ links, setLinks ] = useState<any[]>([])
+    const [ link, setLink ] = useState<LinkWithId>()
     const [ isLoading, setIsLoading ] = useState<boolean>(false)
+    const [ modalVisible, setModalVisible ] = useState<boolean>(false)
     const [ categories, setCategories ] = useState<CategoryPropsItem[]>([])
     const [ linksFiltered, setLinksFiltered ] = useState<any[] | null>([])
-    const [ links, setLinks ] = useState<any[]>([])
-
-
+    const [ pageNameModal, setPageNameModal ] = useState<string | undefined>(undefined)
+    
     async function fetch_Links() {
         if (user && user.access_token_prov) {
             try {
@@ -72,13 +75,6 @@ export default function LinksIndex() {
             }
         } else {
             console.log("Usuário não autenticado") // toast pedindo para fazer login novamente ou chamado do refresh_token
-        }
-    }
-
-    function handleOnDetails_Link(id: string) { 
-        const link = links.find(link => link.id === id)
-        if (link) {
-            console.log('Detalhes do link:', link)
         }
     }
 
@@ -132,14 +128,20 @@ export default function LinksIndex() {
         }
     }
 
- 
+    function ChangeModalVisibility() {
+        setModalVisible(prev => !prev)
+    }
+
     useEffect(() => {
         if (user?.access_token_prov) {
             fetch_Links()
             syncCategories_and_fetchCategories()
         }
     }, [])
-
+    
+    useEffect(() => {
+        console.log("🔗 Link salvo no estado:", link)    
+    }, [link])
  
     useEffect(() => {
         if (!selectedCategory) {
@@ -155,31 +157,68 @@ export default function LinksIndex() {
         }
     }, [selectedCategory, links])  
  
-  
+    function ChangePageNameModal(page: string | undefined) {
+         ChangeModalVisibility()
+         setTimeout(() => {
+            setPageNameModal(page)
+            ChangeModalVisibility()
+       }, 185) 
+    }
+
     return (
-        <View style={styles.container}>
-            {isLoading ? (
-                <>
-                    <LinkSkeleton />
-                    <LinkSkeleton />
-                    <LinkSkeleton />
-                    <LinkSkeleton />
-                    <LinkSkeleton />
-                    <LinkSkeleton />
-                    <LinkSkeleton />
-                    <LinkSkeleton />
-                </> 
-            ) : (
-                    <Links
-                        data={linksFiltered ? linksFiltered : links}
-                        onDelete={handleOnDelete_Link}
-                        onDetails={handleOnDetails_Link}
-                        categories={categories}
-                        selectedCategory={selectedCategory}
-                        setSelectCategory={setSelectCategory}
-                    />
-            )} 
-        </View>
+        <SafeAreaView style={{ flex: 1 }}>
+
+            <View style={styles.container}>
+
+                {isLoading ? (
+                    <>
+                        <LinkSkeleton />
+                        <LinkSkeleton />
+                        <LinkSkeleton />
+                        <LinkSkeleton />
+                        <LinkSkeleton />
+                        <LinkSkeleton />
+                        <LinkSkeleton />
+                        <LinkSkeleton />
+                    </> 
+                ) : (
+                        <Links
+                            data={linksFiltered ? linksFiltered : links}
+                            setLink={setLink}
+                            categories={categories}
+                            onDelete={handleOnDelete_Link}
+                            selectedCategory={selectedCategory}
+                            setSelectCategory={setSelectCategory}
+                            modalOptionsVisiblity={ChangeModalVisibility}
+                        />
+                )} 
+                <ChooseOptionModal 
+                    modalVisible={modalVisible} 
+                    toggleModal={ChangeModalVisibility}
+                    pageNameModal={pageNameModal}
+                    ChangePageNameModal={ChangePageNameModal}
+                >
+                    {pageNameModal ?
+                        <Text>
+                            PAGE NAME MODAL TESTE
+                        </Text>
+                    :    
+                        <View style={styles.content_modal}>
+                            <ActionSelector nameAction='Editar' icon={"pencil"} onPress={() => {
+                                ChangePageNameModal("Editar Link")
+                            }}/>
+                            <ActionSelector nameAction='Resumir com IA' icon={"north-star"} onPress={() => {
+                                ChangePageNameModal("Resumir Link com IA")
+                            }}/>
+                            <ActionSelector nameAction='Deletar' icon={"trash"} colorBack={colors.red[200]} onPress={() => {
+                                ChangeModalVisibility()
+                                handleOnDelete_Link(link?.id!)
+                            }}/>
+                        </View>
+                    }
+                </ChooseOptionModal> 
+            </View>
+        </SafeAreaView>
     )
 }
 
@@ -189,4 +228,13 @@ const styles = StyleSheet.create({
         padding: 5,
         backgroundColor: colors.gray[950],
     },
+    content_modal: {
+        flex: 1,
+        gap: 19,
+        marginTop: -6,
+        marginBottom: 3,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+    }
 })
